61cf4bf304b946c011f1542c8996c988874a1761
amefure <shachi10969@gmail.com>
2023-08-23 19:38:25 +0900

    Initial Commit

---
+++ MyBluetoothTest/MyBluetoothTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBluetoothTestApp.swift
+//  MyBluetoothTest
+//
+//  Created by t&a on 2023/08/23.
+//
+
+import SwiftUI
+
+@main
+struct MyBluetoothTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
